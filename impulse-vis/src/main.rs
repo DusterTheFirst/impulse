40e3459f94d17c65c51a97c0e67b8676d1af6a39
Zachary <me@dusterthefirst.com>
2021-09-19 15:10:28 +0200

    Ground plane

--- impulse-vis/src/main.rs
+++ impulse-vis/src/main.rs
@@ -1,6 +1,6 @@
-use std::error::Error;
+use std::{collections::VecDeque, error::Error, time::Instant};
 
-use three_d::*;
+use three_d::{core::Indices, *};
 
 fn main() -> Result<(), Box<dyn Error>> {
     tracing_subscriber::fmt().pretty().init();
@@ -14,6 +14,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Get the graphics context from the window
     let context = window.gl()?;
+
+    // Create the lighting pipeline
+    let mut pipeline = DeferredPipeline::new(&context)?;
 
     // Create a camera
     let mut camera = Camera::new_perspective(
@@ -43,68 +46,145 @@ fn main() -> Result<(), Box<dyn Error>> {
         },
     )?;
 
-    let ground = Model::new(&context, &CPUMesh::cube())?;
+    let ground = Model::new(
+        &context,
+        &CPUMesh {
+            name: "ground".into(),
+            material_name: None,
+            positions: [
+                vec3(-100.0, 0.0, -100.0),
+                vec3(100.0, 0.0, -100.0),
+                vec3(-100.0, 0.0, 100.0),
+                vec3(100.0, 0.0, 100.0),
+            ]
+            .iter()
+            .flat_map(|v| [v.x, v.y, v.z])
+            .collect(),
+            indices: Some(Indices::U32(vec![0, 1, 3, 0, 2, 3])),
+            normals: Some(
+                [vec3(0.0, 1.0, 0.0), vec3(0.0, 1.0, 0.0)]
+                    .iter()
+                    .flat_map(|v| [v.x, v.y, v.z])
+                    .collect(),
+            ),
+            uvs: None,
+            colors: None,
+        },
+    )?;
+
+    let sunlight = DirectionalLight::new(&context, 1.0, Color::WHITE, &vec3(0.0, 0.0, 1.0))?;
+
+    let mut fps_rolling_average = VecDeque::with_capacity(1000);
+    let mut frame_time_rolling_average = VecDeque::with_capacity(1000);
 
     // Start the main render loop
     window.render_loop(move |mut frame_input: FrameInput| {
+        let frame_start = Instant::now();
+
+        let mut change = frame_input.first_frame;
+
+        if fps_rolling_average.len() == fps_rolling_average.capacity() {
+            fps_rolling_average.pop_back();
+        }
+        fps_rolling_average.push_front(1000.0 / frame_input.elapsed_time);
+
         // Ensure the viewport matches the current window viewport which changes if the window is resized
-        gui.update(&mut frame_input, |gui_context| {
-            use three_d::egui::*;
+        change |= gui
+            .update(&mut frame_input, |gui_context| {
+                use three_d::egui::{plot::*, *};
 
-            Window::new("Visualization Parameters")
-                // .default_size((0.0, 0.0))
-                .show(&gui_context, |ui| {
-                    ui.heading("Camera Position");
+                Window::new("Visualization Properties")
+                    // .default_size((0.0, 0.0))
+                    .show(&gui_context, |ui| {
+                        ui.heading("Camera Position");
+                        ui.label(format!("{:?}", camera.position()));
+                        ui.heading("Camera Target");
+                        ui.label(format!("{:?}", camera.target()));
 
-                    ui.label(format!("{:?}", camera.position()));
-                    ui.label(format!("{:?}", camera.target()));
+                        ui.separator();
+                        ui.heading("Frame Time");
+                        ui.label(format!(
+                            "{} ms",
+                            frame_time_rolling_average.iter().sum::<u128>()
+                                / frame_time_rolling_average.len().max(1) as u128
+                        ));
+                        ui.add(
+                            Plot::new("Frame Time")
+                                .line(
+                                    Line::new(Values::from_values_iter(
+                                        frame_time_rolling_average
+                                            .iter()
+                                            .enumerate()
+                                            .map(|(x, &y)| Value::new(-(x as f64), y as f64)),
+                                    ))
+                                    .highlight(),
+                                )
+                                .allow_zoom(false)
+                                .allow_drag(false)
+                                .include_y(1.0 / 60.0)
+                                .include_y(0)
+                                .view_aspect(2.0),
+                        );
+                        ui.heading("Frame Per Second");
+                        ui.label(format!(
+                            "{:.1}",
+                            fps_rolling_average.iter().sum::<f64>()
+                                / fps_rolling_average.len() as f64
+                        ));
+                        ui.label(format!("{}", fps_rolling_average.len()));
+                        ui.add(
+                            Plot::new("Frame Time")
+                                .line(
+                                    Line::new(Values::from_values_iter(
+                                        fps_rolling_average
+                                            .iter()
+                                            .enumerate()
+                                            .map(|(x, &y)| Value::new(-(x as f64), y)),
+                                    ))
+                                    .highlight(),
+                                )
+                                .allow_zoom(false)
+                                .allow_drag(false)
+                                .include_y(0)
+                                .view_aspect(2.0),
+                        );
 
-                    // ui.allocate_space(ui.available_size());
-                });
-        })
-        .unwrap();
+                        // ui.allocate_space(ui.available_size());
+                    });
+            })
+            .unwrap();
 
-        camera.set_viewport(frame_input.viewport).unwrap();
-        control
+        change |= camera.set_viewport(frame_input.viewport).unwrap();
+        change |= control
             .handle_events(&mut camera, &mut frame_input.events)
             .unwrap();
+
+        if change {
+            pipeline
+                .geometry_pass(&camera, &[(&ground, &material)])
+                .unwrap();
+        }
 
         // Start writing to the screen and clears the color and depth
         Screen::write(
             &context,
-            ClearState::color_and_depth(0.0, 0.0, 0.0, 1.0, 1.0),
+            ClearState::color_and_depth(1.0, 1.0, 1.0, 1.0, 1.0),
             || {
-                // axes.render(&camera)?;
-                ground.render_with_lighting(
-                    &camera,
-                    &material,
-                    LightingModel::Blinn,
-                    Some(&AmbientLight {
-                        color: Color::WHITE,
-                        intensity: 0.1,
-                    }),
-                    &[],
-                    &[&SpotLight::new(
-                        &context,
-                        2.0,
-                        Color::WHITE,
-                        &vec3(0.0, 100000.0, 1.0),
-                        &vec3(0.0, -1.0, 0.0),
-                        f32::MAX,
-                        0.0,
-                        0.0,
-                        0.0,
-                    )?],
-                    &[],
-                )?;
+                axes.render(&camera)?;
                 ground.render_normals(&camera)?;
+                pipeline.light_pass(&camera, None, &[&sunlight], &[], &[])?;
+
+                gui.render()?;
 
                 Ok(())
             },
         )
         .unwrap();
 
-        gui.render().unwrap();
+        if frame_time_rolling_average.len() == frame_time_rolling_average.capacity() {
+            frame_time_rolling_average.pop_back();
+        }
+        frame_time_rolling_average.push_front((Instant::now() - frame_start).as_millis());
 
         // Returns default frame output to end the frame
         FrameOutput::default()
